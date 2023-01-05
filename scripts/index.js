1feab0f5937ac73667fa53c17e9c24d42261892e
Pedro Hiroshi Yoshida Dos Santos <98420552+hiroshi-pedro@users.noreply.github.com>
2023-01-05 00:43:16 -0300

    barra de navegação

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    $('.navigation-cell').click(function() {
+        $('.navigation-mobile').css("display", "flex")
+        $('#navigation-cell-a').css("display", "none")
+        $('#navigation-cell-b').css("display", "block")
+
+        $('html, body').css({
+            'overflow': 'hidden'
+        });
+        
+    })
+
+    $('#navigation-cell-b').click(function() {
+        $('.navigation-mobile').css("display", "none")
+        $('#navigation-cell-a').css("display", "block")
+        $('#navigation-cell-b').css("display", "none")
+        
+        $(window).resize(function() {
+            if ($(window).width() > 519) {
+                $('.navigation-cell').css("display", "none")
+                $('.navigation-mobile').css("display", "none")
+            }
+        })
+
+        $(window).resize(function() {
+            if ($(window).width() < 519) {
+                $('.navigation-mobile').css("display", "none")
+                $('#navigation-cell-a').css("display", "block")
+                $('#navigation-cell-b').css("display", "none")
+            }
+        })
+        $('html, body').css({
+            'overflow': 'auto'
+        });
+    })
+
+})
